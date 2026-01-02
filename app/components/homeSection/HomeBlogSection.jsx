d9843bf18dc70c87c1df9582ffc5bcfd94b86d15
ajayk <158446084+ajayk9xtechnologies@users.noreply.github.com>
2026-01-02 16:58:59 +0400

    Updated Hoem blog section

--- app/components/homeSection/HomeBlogSection.jsx
+++ app/components/homeSection/HomeBlogSection.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import SectionTitle from "../SectionTitle";
+ import SectionTitle from "../SectionTitle";
 
 import { getBaseUrl } from "../../lib/getBaseUrl";
 import { MantisImage } from "../../common";
